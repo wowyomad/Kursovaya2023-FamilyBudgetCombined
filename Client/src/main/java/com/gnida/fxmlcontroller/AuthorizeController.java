812694346936e9c0ef7ad4ae66bd92b5a657aa50
wowyomad <vadimsun8837@gmail.com>
2023-11-29 13:20:50 +0300

    Client fxmlcontrollers package and resources added to commit tracking

---
+++ Client/src/main/java/com/gnida/fxmlcontroller/AuthorizeController.java
@@ -0,0 +1,130 @@
+package com.gnida.fxmlcontroller;
+
+import com.gnida.Client;
+import com.gnida.Main;
+import com.gnida.SceneLoader;
+import com.gnida.converter.Converter;
+import com.gnida.entity.User;
+import com.gnida.model.Request;
+import com.gnida.model.Response;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.nio.channels.NotYetConnectedException;
+
+public class AuthorizeController {
+
+    private static final String loginRegex = "^[a-zA-Z0-9_]+$";
+    private static final String passwordRegex = "^[a-zA-Z\\d@$!%*?&#]+$";
+
+    @FXML
+    private Label errorMessage;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void onExitClicked() {
+        Platform.exit();
+    }
+
+    private final Client client;
+
+
+    public AuthorizeController() {
+        client = Main.getContext().getBean(Client.class);
+
+        if (!client.isConnected()) {
+            throw new NotYetConnectedException();
+        }
+    }
+    @FXML
+    public void onLoginClicked() {
+        if (!areLoginAndPasswordCorrect()) {
+            showIncorrectCredentialsAlert();
+            return;
+        }
+        User user = new User();
+        user.setLogin(loginField.getText());
+        user.setPassword(passwordField.getText());
+        Request request = Request.builder()
+                .type(Request.RequestType.POST)
+                .route(Request.Route.USER)
+                .endPoint("/login")
+                .json(Converter.toJson(user))
+                .build();
+
+        Response response = client.sendRequest(request);
+        Response.Status status = response.getStatus();
+        System.out.println(response);
+        if (status == Response.Status.NOT_FOUND) {
+            errorMessage.setText("Пользователь не найден");
+            errorMessage.setVisible(true);
+        } else {
+            SceneLoader.loadScene(loginButton.getScene(), "/home-view.fxml");
+        }
+    }
+
+    @FXML
+    public void onRegisterClicked() {
+        if (!areLoginAndPasswordCorrect()) {
+            showIncorrectCredentialsAlert();
+            return;
+        }
+        User user = new User();
+        user.setLogin(loginField.getText());
+        user.setPassword(passwordField.getText());
+        Request request = Request.builder()
+                .type(Request.RequestType.POST)
+                .route(Request.Route.USER)
+                .endPoint("/register")
+                .json(Converter.toJson(user))
+                .build();
+        Response response = client.sendRequest(request);
+        Response.Status status = response.getStatus();
+        if (status == Response.Status.CONFLICT) {
+            errorMessage.setText("Введенный вами логин занят");
+            errorMessage.setVisible(true);
+        } else {
+            SceneLoader.loadScene(registerButton.getScene(), "/home-view.fxml");
+
+        }
+
+    }
+
+    private boolean areLoginAndPasswordCorrect() {
+        String login = loginField.getText();
+        String password = passwordField.getText();
+        System.out.println(login);
+        System.out.println(password);
+        return login.matches(loginRegex) && password.matches(passwordRegex);
+
+    }
+
+    @FXML
+    private void hideErrors() {
+        errorMessage.setVisible(false);
+    }
+
+    private void showIncorrectCredentialsAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Неверный ввод");
+        alert.setHeaderText(null);
+        alert.setContentText("Введенный логин или пароль содержат недопустмые символы. Попробуйте еще раз.");
+        alert.showAndWait();
+    }
+
+}
---
+++ Client/src/main/java/com/gnida/fxmlcontroller/HomeController.java
@@ -0,0 +1,42 @@
+package com.gnida.fxmlcontroller;
+
+import com.gnida.SceneLoader;
+import com.gnida.model.Request;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+
+public class HomeController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML Button button1;
+
+    @FXML Button button2;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+        Scene scene = exitButton.getScene();
+        SceneLoader.getPreviousRoot(scene);
+    }
+
+    @FXML
+    void initialize() {
+        button1.setOnAction((it) -> {
+            System.out.println("button1 clcked");
+            Request request = Request.builder()
+                    .type(Request.RequestType.GET)
+                    .endPoint("/booba")
+                    .route(Request.Route.USER)
+                    .build();
+
+        });
+        button2.setOnAction((it) -> {
+            System.out.println("button2 clicked");
+        });
+    }
+
+}
